bee6a935130e090f12da7b36c153cd2eadeb41ae
Manuel Carvalho <burrowsrd@gmail.com>
2020-11-27 21:09:59 +1100

    Added Dictionary Intro part 3

--- dictionaries.py
+++ dictionaries.py
@@ -61,3 +61,13 @@ print(fruit_keys)
 
 fruit["tomato"] = "not nice with ice cream"
 print(fruit_keys)
+print(fruit)
+print(fruit.items())
+f_tuple = tuple(fruit.items())
+print(f_tuple)
+
+for snack in f_tuple:
+    item, description = snack
+    print(item + " is " + description)
+
+print(dict(f_tuple))
